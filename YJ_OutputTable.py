469ac81a7527a6e02062d5812288bfddbad073f9
YYE2022 <114246591+YYE2022@users.noreply.github.com>
2022-12-05 16:55:23 -0500

    Update YJ_OutputTable.py

--- YJ_OutputTable.py
+++ YJ_OutputTable.py
@@ -17,44 +17,67 @@ def main():
     # AnnualMaxHour=[]
 
 
+   def main():
+    cityname=[] #create empty list for each entry
+    latitude=[]
+    longitude=[]
+    # DayOfFocus=[]
+    DayOfFocusHeight=[]
+    # DayofFocusHour=[]
+    # WinterHeight=[]
+    # SummerHeight=[]
+    # WinterHeight=[]
+    # AnnualTotalSunlightHour=[]
+    # AnnualAvgSunlightHour=[]
+    # AnnualMinHour=[]
+    # AnnualMaxHour=[]
+
+
     while True:
-        City_Name=input("enter cityn: ")
-        City_Name=City_Name.upper()  # upper case input, match the cityname in the csv file
-        cityname_list.append(City_Name)  # append user input to cityname list
+        cityn=input("enter cityn: ")
+        cityn=cityn.upper()  # upper case input, match the cityname in the csv file
+        cityname.append(cityn)  # append user input to cityname list
         with open("latlong.csv", newline="")as fo:
             freader=list(csv.reader(fo)) # read the file into a big list
             flag=True
             for row in freader: # for small list in big list
-                if City_Name in row: # if cityname is found in the small list [cityname,latitude,longitude]
+                if cityn in row: # if cityname is found in the small list [cityname,latitude,longitude]
                     flag=False
-                    user_Latitude=row[1] #latitude would be at position [1]
-                    latitude_list.append(user_Latitude) #append latitude to latitude list
-                    user_Longitude=row[2] #longitude would be at position [2]
-                    longitude_list.append(user_Longitude) #append longitude to longitude list
+                    filelatitude=row[1] #latitude would be at position [1]
+                    filelatitude=float(filelatitude)
+                    latitude.append(filelatitude) #append latitude to latitude list
+                    filelongi=row[2] #longitude would be at position [2]
+                    filelongi=float(filelongi)
+                    longitude.append(filelongi) #append longitude to longitude list
                     break
 
             if flag:
-                user_Latitude=input("not found, enter lat: ") # if cityname is not found in the small list, ask for input manually
-                latitude_list.append(user_Latitude) #append input_latitude to latitude list
-                user_Longitude=input("not found, enter long: ")
-                longitude_list.append(user_Longitude)#append input_longitude to longitude list
+                filelatitude=float(input("not found, enter lat: ")) # if cityname is not found in the small list, ask for input manually
+                latitude.append(filelatitude) #append input_latitude to latitude list
+                filelongi=float(input("not found, enter long: "))
+                longitude.append(filelongi)#append input_longitude to longitude list
     
-             for index in range(1):
+            #print(cityname,latitude,longitude)
+            for index in range(1):
                 filelatitude = latitude[index]        
                 mheight=bheight(filelatitude)
                 DayOfFocusHeight.append(mheight)  
+
+
         end = input("Do you want to stop entering values (Y/N)? ") 
         print()
         if  end.upper() == 'Y' :
             break
 
+    myheader=['CityName','Latitude','Longitude','DayOfFocus','DayOfFocusHeight','DayofFocusHour','WinterHeight','SummerHeight','WinterHeight','AnnualTotalSunlightHour','AnnualAvgSunlightHour','AnnualMinHour','AnnualMaxHour']
 
-    myheader=['City_Name','Latitude','Longitude','DayOfFocus','DayOfFocusHeight','DayofFocusHour','WinterHeight','SummerHeight','AnnualTotalSunlightHour','AnnualAvgSunlightHour','AnnualMinHour','AnnualMaxHour']
-    with open('output.csv','w',newline='') as newfile: #csv file name can be changed later, when we finalize it
+    with open('output.csv','w',newline='') as newfile:
         writer=csv.writer(newfile)
         writer.writerow(myheader)
         for i in range(len(latitude)):
             writer.writerow([cityname[i],latitude[i],longitude[i],DayOfFocusHeight[i]])
+
+
 
 if __name__ == '__main__':
     main()
