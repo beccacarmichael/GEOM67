38d04c68e06ac94b1118f4aa8f08baf3658591d7
chris-dg-b <chris.broom@hotmail.com>
2022-12-06 14:32:31 -0500

    fix

--- FullScript.py
+++ FullScript.py
@@ -224,7 +224,7 @@ def sunlighthourstatistics (user_Latitude):
         
     CalculationsList=[]
     for d in range(1,365):
-        sunlighthour=2*2*(1/15)*(180/math.pi)*math.acos((-math.tan((math.pi/180)*43.6))*(math.tan((math.pi/180)*23.44*math.sin((360/365)*(d+284)*(math.pi/180)))))
+        sunlighthour=2*(1/15)*(180/math.pi)*math.acos((-math.tan((math.pi/180)*43.6))*(math.tan((math.pi/180)*23.44*math.sin((360/365)*(d+284)*(math.pi/180)))))
         CalculationsList.append(sunlighthour)
 
     #min,max,avg,sum
